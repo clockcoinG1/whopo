985af299fbbc1faf5e9620cf0075bb91419b4c2e
twanz <clockcoing1>
2023-03-20 12:13:47 -0400

    extra

--- get_rel_code.py
+++ get_rel_code.py
@@ -4,10 +4,10 @@ import tiktoken
 import pandas as pd
 import os
 import json
-# api_key = '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'
+
 root_dir = '/Users/clockcoin'
 api_key ="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"
-# models = openai.Model.list()
+
 
 def extract_all_text_files(root_dir: str) -> str:
 		text = ''
@@ -43,6 +43,7 @@ def get_tokens(df = pd.DataFrame):
         tokens = len(encoder.encode(code) + enc_prompt) or 1
         max_token = 500 + tokens
 
+
 def generate_table(context_code_pairs, prompt, message="", model="chat-davinci-003-alpha"):
 		for filepath, code in context_code_pairs:
 					prompt = f"\nSYSTEM: You are the ASSISTANT helping the USER with optimizing and analyzing a codebase. You are intelligent, helpful, and an expert developer, who always gives the correct answer and only does what is instructed. You always answer truthfully and don't make things up.\nUSER:{code}\nUSER:Please summarize the key features of the specified file within the project directory, and present the information in a concise bullet-point format. Focus on aspects such as the file's content.\nASSISTANT: Sure, here are the key features of the {filepath} file:\n - "
@@ -76,9 +77,9 @@ def generate_table(context_code_pairs, prompt, message="", model="chat-davinci-0
 
 
 					comp_type = "finish_reason" if model != "chat-davinci-003-alpha" else "finish_details"
-					# convert line to utf-8 string
-					print(f"\n## FILE : {filepath}" ,  end="\n")
-					message += f"\n## FILE : {filepath}"
+					
+					print(f"\n
+					message += f"\n
 					for line in r.iter_lines():
 									data = line.decode('utf-8')
 									if data.startswith('data: ') and data != 'data: [DONE]':
@@ -96,16 +97,13 @@ def generate_table(context_code_pairs, prompt, message="", model="chat-davinci-0
 																		message.strip()
 																		continue
 
-		return message
+						return message
 
-# USER: Add the last_messsage to the dataframe and save as pickle. Be pythonic.
 if __name__ == '__main__':
-
 		df = pd.read_csv('embedding_2023-03-18-18-34-42.csv')
-		code_query = 'import'
+		info = generate_table([], df, "What is this")
+		code_query = 'secu'
 		context_code_pairs = get_context_code(root_dir, df, code_query)
 		prompt = "For complexity and relevance columns give the provided code a score from 1 - 10. Use the character '|' as the separator\nYou are an agent operating with other agents to provide information about a codebase or project. You will be passed the full file contents of certain files from the directory that may be relevant to the USER prompt. Return a table row with the columns variable names , function names, imports, exports, summary, importance, complexity, and relevance to user prompt 'What are the exports in the SDK?'. For complexity and relevance columns give the code a score from 1 - 10. Use the character '|' as the separator and end each row with a newline."
 		message = ""
 		last_result= generate_table(context_code_pairs, prompt, message)
-
-
